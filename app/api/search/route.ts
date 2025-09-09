6ab15d589dd328f898c3297a56224f1727f3f6fe
ShashwatM3 <smahalanobis@ucdavis.edu>
2025-09-09 13:07:12 +0400

    placeholder routes

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,1 +1,23 @@
 // For SEARCHING across a CLOUD
+
+import { NextRequest, NextResponse } from 'next/server';
+
+export const dynamic = "force-dynamic";
+
+export async function POST(req: NextRequest) {
+  try {
+  
+    const body = await req.json();
+    const metric_1 = body.metric_1;
+    return NextResponse.json({ response: "" });
+    
+  } catch(error: any) {
+  
+    console.error("Error in API:", error);
+    return NextResponse.json(
+      { error: "Failed to check: " + error.message }, 
+      { status: 500 }
+    );
+    
+  }
+}
